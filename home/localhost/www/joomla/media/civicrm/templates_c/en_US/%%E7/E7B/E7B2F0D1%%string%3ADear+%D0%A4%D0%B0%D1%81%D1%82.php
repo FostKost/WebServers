b7f3a4d75a45810908792411106cd6d73ef70bde
Fast Konstantin <FostKost@gmail.com>
2015-11-30 18:26:42 +0500

    Fabrik
    Fabrik add

--- home/localhost/www/joomla/media/civicrm/templates_c/en_US/%%E7/E7B/E7B2F0D1%%string%3ADear+%D0%A4%D0%B0%D1%81%D1%82.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.27, created on 2015-11-23 17:53:22
-         compiled from string:Dear+%D0%A4%D0%B0%D1%81%D1%82 */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('block', 'crmScope', 'string:Dear Фаст', 1, false),)), $this); ?>
-<?php $this->_tag_stack[] = array('crmScope', array('extensionKey' => "")); $_block_repeat=true;smarty_block_crmScope($this->_tag_stack[count($this->_tag_stack)-1][1], null, $this, $_block_repeat);while ($_block_repeat) { ob_start(); ?>Dear Фаст<?php $_block_content = ob_get_contents(); ob_end_clean(); $_block_repeat=false;echo smarty_block_crmScope($this->_tag_stack[count($this->_tag_stack)-1][1], $_block_content, $this, $_block_repeat); }  array_pop($this->_tag_stack); ?>
