d9d66e692f1c3d08994f0643d540b0ad9ccafdda
Fast Konstantin <FostKost@gmail.com>
2016-09-02 09:43:15 +0500

    skripts1

--- home/localhost/www/skripts/index.php
+++ home/localhost/www/skripts/index.php
@@ -1,11 +1,31 @@
 ﻿<html>
     <head>
 
-      <script type="text/javascript">
+      <script type="text/javascript">//Функция нажатия на кнопку "создать Vlan"
                 function cre() {
-                  alert(document.getElementById("vlan").value);
-
+                  var vlan=document.getElementById("vlan").value;
+                  var url=window.location.href.replace(window.location.search,'')+'?type=create&vlan='+vlan;
+                  window.location.href = url
                 }
+                function del() {
+                  var vlan=document.getElementById("vlan").value;
+                  var url=window.location.href.replace(window.location.search,'')+'?type=delete&vlan='+vlan;
+                  window.location.href = url
+                }
+                function add() {
+                  var vlan=document.getElementById("vlan").value;
+                  var port=document.getElementById("port").value;
+                  var url=window.location.href.replace(window.location.search,'')+'?type=add_on_port&vlan='+vlan+'&port='+port;
+                  window.location.href = url
+                }
+                function del_from() {
+                  var vlan=document.getElementById("vlan").value;
+                  var port=document.getElementById("port").value;
+                  var url=window.location.href.replace(window.location.search,'')+'?type=delete_on_port&vlan='+vlan+'&port='+port;
+                  window.location.href = url
+                }
+                function sleep (m) {var then = new Date(new Date().getTime() + m); while (new Date() < then) {}}
+
              </script>
 
 
@@ -130,19 +150,80 @@
               <input type="button" class="btn btn-inverse" onclick="cre()"   name="create_vlan" value="Создать Vlan" >
               <br>
               <br>
-              <input type="button" class="btn btn-inverse"   name="delete_vlan" value="Удалить Vlan ">
+              <input type="button" class="btn btn-inverse" onclick="del()"  name="delete_vlan" value="Удалить Vlan ">
               <br>
               <br>
-              <input type="button" class="btn btn-inverse"   name="add_vlan_ports" value=" Добавить tagget vlan на порт " >
+              <input type="button" class="btn btn-inverse" onclick="add()"   name="add_vlan_ports" value=" Добавить tagget vlan на порт " >
               <br>
               <br>
-              <input type="button" class="btn btn-inverse" "  name="delete_vlan_ports" value=" Удалить Vlan с порта">
+              <input type="button" class="btn btn-inverse" onclick="del_from()" "  name="delete_vlan_ports" value=" Удалить Vlan с порта">
               </div>
           <form>
 
 
 
        <?php
+       if (isset($_GET['type']))
+{
+    if ($_GET['type']=="create")//скрипт на создание вланы
+    {
+      $cmd="start C:/WebServers/home/localhost/www/skripts/ruby/eogs/create_vlan.rb ".$_GET['vlan'];
+      echo $cmd;
+      echo "<script language='javascript'>\n";
+      echo "alert('Идет создание Vlan...')";
+      echo "</script>\n";
+      exec ( $cmd );
+      echo "<script language='javascript'>\n";
+      echo "setTimeout(\"alert('Vlan создана!!!')\",9000)";
+      echo "</script>\n";
+    }
+
+    if ($_GET['type']=="delete")//скрипт на удаление вланы
+    {
+      $cmd="start C:/WebServers/home/localhost/www/skripts/ruby/eogs/delete_vlan.rb ".$_GET['vlan'];
+      echo "<script language='javascript'>\n";
+      echo "alert('Удаление Vlan...')";
+      echo "</script>\n";
+      exec ( $cmd );
+      echo "<script language='javascript'>\n";
+      echo "setTimeout(\"alert('Vlan удалена!!!')\",9000)";
+      echo "</script>\n";
+
+    }
+
+    if ($_GET['type']=="delete_on_port")//скрипт на удаление вланы с порта
+    {
+      $cmd="start C:/WebServers/home/localhost/www/skripts/ruby/eogs/delet_vlan_ports.rb ".$_GET['vlan']." ".$_GET['port'];
+      echo "<script language='javascript'>\n";
+      echo "alert('Удаление Vlan с портов...')";
+      echo "</script>\n";
+      echo "<script language='javascript'>\n";
+      echo "setTimeout(\"alert('Vlan удалена с портов!!!')\",9000)";
+      echo "</script>\n";
+      exec ( $cmd );
+    }
+
+    if ($_GET['type']=="add_on_port")//скрипт на добавление вланы на порт.
+    {
+      $cmd="start C:/WebServers/home/localhost/www/skripts/ruby/eogs/add_vlan_ports.rb ".$_GET['vlan']." ".$_GET['port'];
+      echo "<script language='javascript'>\n";
+      echo "alert('Добавление Vlan на порты...')";
+      echo "</script>\n";
+      exec ( $cmd );
+      echo "<script language='javascript'>\n";
+      echo "setTimeout(\"alert('Vlan добавлена на порты!!!')\",9000)";
+      echo "</script>\n";
+    }
+
+
+}
+
+else
+{
+  echo "<script language='javascript'>\n";
+  echo "alert('тестовая версия без авторизации')";
+  echo "</script>\n";
+}
 
        ?>
 
